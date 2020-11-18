32a803841a903d28c01a3ad8cd512ad88ee4d548
Jerry Kuo <yukuo@umich.edu>
2020-11-18 12:26:34 -0500

    get like dislike count

--- search_videos.py
+++ search_videos.py
@@ -1,12 +1,11 @@
 from googleapiclient.discovery import build
 
 #enter your youtube api key
-youtube_api_key = ''
+youtube_api_key = 'AIzaSyACtD8D34hwm44lqQU_gwoTL5207HMWls8'
+#build the service object
+youtube_service = build('youtube','v3',developerKey=youtube_api_key)
 
-def search_videos(q,max_results=10,order='relevance',token=None,location=None,location_radius=None):
-
-    #build the service object
-    youtube_service = build('youtube','v3',developerKey=youtube_api_key)
+def search_videos(q,max_results=5,order='relevance',token=None,location=None,location_radius=None):
 
     search_response = youtube_service.search().list(
         q=q,
@@ -25,8 +24,33 @@ def search_videos(q,max_results=10,order='relevance',token=None,location=None,lo
         if result['id']['kind'] == 'youtube#video':
             videos.append(result)
     
+    
+
     return videos
 
-print(search_videos('League of Legends'))
+# item['id']['videoId']
+def get_likes_dislikes(id):
 
+    search_response = youtube_service.videos().list(
+        id = id,
+        part = "id,snippet,statistics",
+    ).execute()
 
+    video_likes = search_response['items'][0]['statistics']['likeCount']
+    video_dislikes = search_response['items'][0]['statistics']['dislikeCount']
+    
+    return (video_likes,video_dislikes)
+
+def create_video_list(video_list):
+    videos_list = []
+    video_data = {}
+    for video in video_list:
+        video_data['title'] = video['snippet']['title']
+        ldc = get_likes_dislikes(video['id']['videoId'])
+        video_data['likes'] = ldc[0]
+        video_data['dislikes'] = ldc[1]
+        videos_list.append(video_data)
+    
+    return videos_list
+
+print(create_video_list(search_videos('League of Legends')))
